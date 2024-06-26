7852e5b0b528a881397db59746a48a0b1d573e56
javassar <160563445+javassar@users.noreply.github.com>
2024-06-25 21:08:27 -0400

    created control group free response json

---
+++ control_intervention_json.js
@@ -0,0 +1,7 @@
+var control_free_response_question_json = {
+    elements: [{
+                    "type": "comment",
+                    "name": "control-free-response",
+                    "title": "Please freely reflect in the space below on the statement: <relevantRow>",
+                    "isRequired": false
+    }]}
