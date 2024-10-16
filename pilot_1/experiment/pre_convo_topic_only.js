202d4d5a4a7abba80883405868c4dbbc23b6e577
javassar <160563445+javassar@users.noreply.github.com>
2024-10-16 14:15:27 -0700

    new html & js for new convo-focused pilot

---
+++ pilot_1/experiment/pre_convo_topic_only.js
@@ -0,0 +1,72 @@
+var pre_convo_topic_only_json = {
+    elements: [
+      {
+        type: "matrix",
+        name: "topic",
+        title: "Imagine you are about to engage in a conversation with a small group of people where differing opinions exist. Rate how comfortable you would be talking about each of the following topics.",
+        isRequired: true,
+        columns: [
+          {
+            value: 1,
+            text: "Strongly uncomfortable",
+          },
+          {
+            value: 2,
+            text: "Moderately uncomfortable",
+          },
+          {
+            value: 3,
+            text: "A little uncomfortable",
+          },
+          {
+            value: 4,
+            text: "Neutral",
+          },
+          {
+            value: 5,
+            text: "A little comfortable",
+          },
+          {
+            value: 6,
+            text: "Moderately comfortable",
+          },
+          {
+            value: 7,
+            text: "Strongly comfortable",
+          },
+        ],
+        rows: [
+          {
+            value: "human euthanasia in the U.S.",
+            text: "Human euthanasia in the U.S.",
+          },
+          {
+            value: "gender equality in the U.S.",
+            text: "Gender equality in the U.S.",
+          },
+          {
+            value: "the role of the U.S. government in healthcare",
+            text: "The role of the U.S. government in healthcare",
+          },
+          {
+            value: "the atomic bombings of Hiroshima and Nagasaki",
+            text: "The atomic bombings of Hiroshima and Nagasaki",
+          },
+          {
+            value: "mandating vaccines in the U.S.",
+            text: "Mandating vaccines in the U.S.",
+          },
+          {
+            value: "the criminal justice system in the U.S.",
+            text: "The criminal justice system in the U.S.",
+          },
+          {
+            value: "same-sex marriage in the U.S.",
+            text: "Same-sex marriage in the U.S.",
+          },
+        ],
+        rowsOrder: "random",
+      },
+    ],
+  };
+  
