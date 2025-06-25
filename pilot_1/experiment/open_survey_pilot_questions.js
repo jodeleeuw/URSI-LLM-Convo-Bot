98ff3076307970ec316f5da27c5c1a7c3102e5ec
javassar <160563445+javassar@users.noreply.github.com>
2025-06-25 11:50:51 -0700

    created pilot for willingness questions
    includes demographic questions

--- pilot_1/experiment/open_survey_pilot_questions.js
+++ pilot_1/experiment/open_survey_pilot_questions.js
@@ -1,86 +1,235 @@
-
-var open_survey_pilot_questions_json = {
-    elements: [
-{
-    type: "matrix",
-    name: "question1",
-    title: "Imagine you are about to engage in a conversation with a small group of people where differing opinions exist. Rate how much you agree with the following statements.",
-    isRequired: true,
-    columns: [
-      {
-        value: 1,
-        text: "Disagree strongly",
-      },
-      {
-        value: 2,
-        text: "Disagree moderately",
-      },
-      {
-        value: 3,
-        text: "Disagree a little",
-      },
-      {
-        value: 4,
-        text: "Neither agree nor disagree",
-      },
-      {
-        value: 5,
-        text: "Agree a little",
-      },
-      {
-        value: 6,
-        text: "Agree moderately",
-      },
-      {
-        value: 7,
-        text: "Agree strongly",
-      },
-    ],
-    rows: [
-      {
-        value:
-          "value",
-        text: "I would feel excited to join the conversation",
-      },
-      {
-        value:
-          "value",
-        text: "I would feel anxiety or dread",
-      },
-      {
-        value:
-          "value",
-        text: "I would look forward to hearing what others have to say on the topic",
-      },
-      {
-        value:
-          "value",
-        text: "I would prefer to listen to the others discuss and not chime in",
-      },
-      {
-        value:
-          "value",
-        text: "I would enjoy the conversation if I knew that the conversation would not become heated",
-      },
-      {
-        value:
-          "value",
-        text: "I would prefer to only share my opinion and not have other's competing perspectives in the conversation",
-      },
-      {
-        value:
-          "value",
-        text: "I would enjoy the conversation if it were purely meant to be a learning opportunity and there were no expectations for people's opinions to change.",
-      },
-      {
-        value:
-          "value",
-        text: "I would feel anxiety or dread",
-      },
-    ],
-    rowsOrder: "random",
-  },
-    ]
-}
-
-// then have conditional follow ups that ask why they feel that way in free response 
+var open_survey_json = {
+  elements: [
+    {
+      type: "checkbox",
+      name: "commitment-check",
+      title: "The quality of our survey data plays an integral role in the accuracy of our experiment results. To draw accurate conclusions, it is important to us that you provide thoughtful answers to each survey question. Do you commit to providing thoughtful answers in the questions in this survey?",
+      isRequired: true,
+      choices: [
+        {
+          value: "maybe",
+          text: "I can't promise either way",
+        },
+        {
+          value: "yes",
+          text: "Yes, I do",
+        },
+        {
+          value: "no",
+          text: "No, I do not",
+        },
+      ],
+    },
+    {
+      type: "checkbox",
+      name: "ethnicity",
+      title: "What is your ethnicity? (Select all that apply)",
+      isRequired: true,
+      choices: [
+        {
+          value: "White",
+          text: "White",
+        },
+        {
+          value: "Black or African American",
+          text: "Black or African American",
+        },
+        {
+          value: "Hispanic or Latino",
+          text: "Hispanic or Latino",
+        },
+        {
+          value: "Asian",
+          text: "Asian",
+        },
+        {
+          value: "Native American or Alaska Native",
+          text: "Native American or Alaska Native",
+        },
+        {
+          value: "Middle Eastern or North African",
+          text: "Middle Eastern or North African",
+        },
+        {
+          value: "Native Hawaiian or Other Pacific Islander",
+          text: "Native Hawaiian or Other Pacific Islander",
+        },
+      ],
+      showOtherItem: true,
+      otherText: "Other (please specify)",
+    },
+    {
+      type: "radiogroup",
+      name: "political-affiliation",
+      title: "What is your political affiliation? (Select one)",
+      isRequired: true,
+      choices: [
+        {
+          value: "Republican",
+          text: "Republican",
+        },
+        {
+          value: "Democrat",
+          text: "Democrat",
+        },
+        {
+          value: "Independent",
+          text: "Independent",
+        },
+      ],
+      showOtherItem: true,
+      otherText: "Other (please specify)",
+    },
+    {
+      type: "text",
+      name: "gender-identification",
+      title: "With which gender do you most identify? (Fill in)",
+      isRequired: true,
+    },
+    {
+      type: "radiogroup",
+      name: "education-level",
+      title: "What is the highest level of education that you have completed? (Select one)",
+      isRequired: true,
+      choices: [
+        {
+          value: "Less than high school",
+          text: "Less than high school",
+        },
+        {
+          value: "High school diploma or GED",
+          text: "High school diploma or GED",
+        },
+        {
+          value: "Some college; no degree",
+          text: "Some college, no degree",
+        },
+        {
+          value: "Associate degree",
+          text: "Associate degree",
+        },
+        {
+          value: "Bachelor's degree",
+          text: "Bachelor's degree",
+        },
+        {
+          value: "Master's degree",
+          text: "Master's degree",
+        },
+        {
+          value: "Doctoral degree (PhD; EdD; etc.)",
+          text: "Doctoral degree (PhD, EdD, etc.)",
+        },
+        {
+          value: "Professional degree (JD; MD; etc.)",
+          text: "Professional degree (JD, MD, etc.)",
+        },
+      ],
+    },
+    {
+      type: "text",
+      name: "religious-affiliation",
+      title: "What religion do you most identify with? (Fill in)",
+      isRequired: true,
+    },
+    {
+      type: "radiogroup",
+      name: "political-ideology",
+      title: "How would you describe your political ideology? (Select one)",
+      isRequired: true,
+      choices: [
+        {
+          value: "Very conservative",
+          text: "Very conservative",
+        },
+        {
+          value: "Conservative",
+          text: "Conservative",
+        },
+        {
+          value: "Somewhat conservative",
+          text: "Somewhat conservative",
+        },
+        {
+          value: "Moderate",
+          text: "Moderate",
+        },
+        {
+          value: "Somewhat liberal",
+          text: "Somewhat liberal",
+        },
+        {
+          value: "Liberal",
+          text: "Liberal",
+        },
+        {
+          value: "Very liberal",
+          text: "Very liberal",
+        },
+      ],
+    },
+    {
+      type: "matrix",
+      name: "topic",
+      title: "Imagine you are about to engage in a conversation with a small group of people where differing opinions exist. For each of the following potential conversation topics, rate how willing you would be to join the conversation.",
+      isRequired: true,
+      columns: [
+        {
+          value: 1,
+          text: "Strongly uncomfortable",
+        },
+        {
+          value: 2,
+          text: "Moderately uncomfortable",
+        },
+        {
+          value: 3,
+          text: "A little uncomfortable",
+        },
+        {
+          value: 4,
+          text: "Neutral",
+        },
+        {
+          value: 5,
+          text: "A little comfortable",
+        },
+        {
+          value: 6,
+          text: "Moderately comfortable",
+        },
+        {
+          value: 7,
+          text: "Strongly comfortable",
+        },
+      ],
+      rows: [
+        {
+          value: "human euthanasia in the U.S.",
+          text: "Human euthanasia in the U.S.",
+        },
+        {
+          value: "gender equality in the U.S.",
+          text: "Gender equality in the U.S.",
+        },
+        {
+          value: "the role of the U.S. government in healthcare",
+          text: "The role of the U.S. government in healthcare",
+        },
+        {
+          value: "mandating vaccines in the U.S.",
+          text: "Mandating vaccines in the U.S.",
+        },
+        {
+          value: "the criminal justice system in the U.S.",
+          text: "The criminal justice system in the U.S.",
+        },
+        {
+          value: "same-sex marriage in the U.S.",
+          text: "Same-sex marriage in the U.S.",
+        },
+      ],
+      rowsOrder: "random",
+    },
+  ],
+};
